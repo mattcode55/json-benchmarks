98ba5b6cba80f98a74b0f38ebe9f1b71306bc891
Matt Smith <matt@mattcode.me>
2017-08-05 11:21:32 +0100

    Add benchmarks for Gson

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ Tested on a quiet system with following specs:
 Benchmark                           Mode  Cnt        Score       Error  Units
 FastjsonBenchmark.benchmarkDecode  thrpt   30   848933.590 ±  8829.303  ops/s
 FastjsonBenchmark.benchmarkEncode  thrpt   30  2294795.689 ± 22700.557  ops/s
+GsonBenchmark.benchmarkDecode      thrpt   30  1004904.229 ±  9308.420  ops/s
+GsonBenchmark.benchmarkEncode      thrpt   30   968026.305 ± 11180.387  ops/s
 JacksonBenchmark.benchmarkDecode   thrpt   30  1431939.227 ± 31333.794  ops/s
 JacksonBenchmark.benchmarkEncode   thrpt   30  2207671.946 ± 27725.673  ops/s
 JsonOrgBenchmark.benchmarkDecode   thrpt   30   577231.263 ± 20107.804  ops/s
---
+++ src/main/java/net/rapidhashing/jsonbenchmarks/GsonBenchmark.java
@@ -0,0 +1,34 @@
+package net.rapidhashing.jsonbenchmarks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonBenchmark {
+	private static final Gson GSON = new GsonBuilder().create();
+	private static final Map<String, Object> PARAMS = new HashMap<>();
+	private static final String TEST_INPUT = "{\"jsonrpc\": \"2.0\", \"method\": \"login\", \"params\": {\"username\": \"3495843958\", \"password\": \"hunter2\", \"user-agent\": \"not-a-botnet-0.1\"}, \"id\": 1}";
+
+	static {
+		PARAMS.put("username", "3495843958");
+		PARAMS.put("password", "hunter2");
+		PARAMS.put("user-agent", "not-a-botnet-0.1");
+	}
+
+	@Benchmark
+	public void benchmarkDecode(Blackhole bh) throws Exception {
+		JsonRpcCall result = GSON.fromJson(TEST_INPUT, JsonRpcCall.class);
+		bh.consume(result);
+	}
+
+	@Benchmark
+	public void benchmarkEncode(Blackhole bh) throws Exception {
+		JsonRpcCall call = new JsonRpcCall("2.0", "login", 1, PARAMS);
+		bh.consume(GSON.toJson(call));
+	}
+}
